2df9939217b0f28d81d4214c1fcb7e8d763c5708
Tania Whiting <tanwhiting@aol.com>
2018-10-09 16:15:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,5 @@
 
 def prime?(number)
-<<<<<<< HEAD
   range_numbers = (1..number).to_a
   remainders = []
   if number < 2  
@@ -18,21 +17,4 @@ def prime?(number)
     else 
       return false 
   end
-=======
-  remainders = []
-  if number < 2  
-    return false
-  else
-    (1..number).each do |element| 
-      remainders = number%element 
-  end
-end
-  zero_remainders = remainders.select do |num|
-      num == 0 
-  end
-  if remainders.size == 2
-      return true  
-    else return false 
-end
->>>>>>> 1a8dd4903eed152a67c4e9a1236f56d0b02b124f
 end
